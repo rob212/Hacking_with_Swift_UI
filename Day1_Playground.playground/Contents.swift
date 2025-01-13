f2892a06cb50c3a008cee4a1ee0e4fc31fceffce
rob212 <robertmcbryde@Robs-Mac-mini.local>
2025-01-13 19:40:33 +0000

    initial commint - day 1

---
+++ Day1_Playground.playground/Contents.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+var greeting = "Hello, playground"
+
+var name = "Ted"
+name = "Rebecca"
+name = "Keeley"
+
+let character = "Daphne"
+
+var playerName = "Roy"
+print(playerName)
+
+
+let managerName = "Michael Scott"
